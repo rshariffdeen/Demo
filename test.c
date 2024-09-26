741bf856085e2536f874b2fb3b77764b2ce1d214
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2024-09-27 02:02:39 +0800

    add bug

--- test.c
+++ test.c
@@ -9,14 +9,9 @@ void bad() {
     arr[0] = 3;
     printf("%d %d %d", arr[0], arr[1], arr[2]);
     int val = 0;
-    if (x + y + 5 < n) {
-        val = arr[x+y+5];
-    } else {
-        printf("Index out of bounds\n");
-        val = -1; // or handle the error appropriately
-    }
+    val = arr[x+y+5];
     printf("testing\n");
-    return 0;
+    return
 }
 
 int main() {
